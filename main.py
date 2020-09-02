5ec706b9385a32a553b16f73b3b0aa77b984ba92
Alejandro Sanz Sanchez <sanzsana@alumni.uv.es>
2020-09-02 14:15:38 +0200

    last fix

--- main.py
+++ main.py
@@ -383,7 +383,6 @@ function table_to_csv(source) {
     const columns = Object.keys(source.data)
     const nrows = source.get_length()
     const lines = [columns.join(',')]
-
     for (let i = 0; i < nrows; i++) {
         let row = [];
         for (let j = 0; j < columns.length; j++) {
@@ -394,12 +393,9 @@ function table_to_csv(source) {
     }
     return lines.join('\\n').concat('\\n')
 }
-
-
 const filename = 'data_result.csv'
 filetext = table_to_csv(source)
 const blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' })
-
 //addresses IE
 if (navigator.msSaveBlob) {
     navigator.msSaveBlob(blob, filename)
